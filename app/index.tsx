ec1b845d4faadeaeb753ee67e0f027ce53b11096
Sandeep Singh <snayal50@gmail.com>
2025-07-17 14:49:35 +0530

    create home page

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,86 @@
-import { Text, View } from "react-native";
- 
+import { images, offers } from "@/constants";
+import { Fragment } from "react";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import cn from "clsx";
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-800 font-quicksand-bold">
-        Welcome to My react Native App
-      </Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      {/* FlatList */}
+      <FlatList
+        data={offers}
+        keyExtractor={(item, index) => `${item.id || index}`}
+        renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0;
+
+          return (
+            <View className="mb-5">
+              <Pressable
+                className={cn(
+                  "offer-card",
+                  isEven ? "flex-row-reverse" : "flex-row"
+                )}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: "#fffff22" }}
+              >
+                {({ pressed }) => (
+                  <Fragment>
+                    <View className="h-full w-1/2">
+                      <Image
+                        source={item.image}
+                        className="size-full"
+                        resizeMode="contain"
+                      />
+                    </View>
+
+                    <View
+                      className={cn(
+                        "offer-card__info",
+                        isEven ? "pl-10" : "pr-10"
+                      )}
+                    >
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className="size-10"
+                        resizeMode="contain"
+                        tintColor="#ffffff"
+                      />
+                    </View>
+                  </Fragment>
+                )}
+              </Pressable>
+            </View>
+          );
+        }}
+        contentContainerStyle={{ paddingBottom: 112, paddingHorizontal: 20 }}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={() => (
+          <View className="flex-between flex-row w-full my-5 ">
+            <View className="flex-start">
+              <Text className="small-bold text-primary">DELIVER TO</Text>
+              <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+                <Text className="paragraph-bold text-dark-100">Lucknow</Text>
+                <Image
+                  source={images.arrowDown}
+                  className="size-3"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+            <Text>Cart</Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
